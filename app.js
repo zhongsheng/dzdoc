6a6fb0f44f80bf60fbc683f85e984ba6586fd10b
Bell <nily.zhong@gmail.com>
2018-09-01 18:35:54 +0800

    show more product information

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const http = require('./utils/http.js')
 
 App({
   onLaunch: function (options) {
+    
     wx.hideTabBar({})
-
+    var thisApp = this;
     wx.getSetting({
       success: res => {
+        wx.getSystemInfo({
+          success: function(res) {
+            if(res.brand == 'devtools'){
+              thisApp.requestUrl = "http://127.0.0.1"
+            }
+          },
+        })
+        
         if (res.authSetting['scope.userInfo']) {
           wx.getUserInfo({
             success: res => {
@@ -24,8 +33,8 @@ App({
     })
   },
 
-  requestUrl: 'https://crm.dzunion.cn',
-
+  requestUrl: "https://crm.dzunion.cn",
+  
   authData: {
     user_id: '',
     username: '',
